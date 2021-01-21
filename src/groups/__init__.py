dfbe27179e6607e8e99abc0ea0e2a93941d9780b
Art <artvanliere@gmail.com>
2021-01-21 16:46:28 +0100

    Added init files

---
+++ src/groups/__init__.py
@@ -0,0 +1,4 @@
+from .Group import Group
+from .SO import SO
+from .SO2 import SO2
+from .SO3 import SO3
---
+++ src/structures/__init__.py
@@ -0,0 +1,3 @@
+from .Vector import Vector
+from .Vector2 import Vector2
+from .Vector3 import Vector3
