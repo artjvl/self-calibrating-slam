ae377d2f7dc570c540cb2fb0514a3ebf92d27f41
Art <artvanliere@gmail.com>
2021-01-29 14:51:16 +0100

    Added test-cases

---
+++ src/test_cases/random_trajectory/Simulator.py
@@ -0,0 +1,5 @@
+import src.framework as framework
+
+
+class Simulator(framework.simulation.Simulation):
+    pass
---
+++ src/test_cases/tutorial_slam2d/Simulator.py
@@ -0,0 +1,7 @@
+import src.framework as framework
+
+
+class Simulator(framework.simulation.Simulation):
+    pass
+
+
