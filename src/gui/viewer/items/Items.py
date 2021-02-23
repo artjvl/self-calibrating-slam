c78e4bfb0ad51cf6a19d444c6e8ba6470567a235
Art <artvanliere@gmail.com>
2021-02-23 11:42:04 +0100

    *Fixed* imports - code now runs!

--- src/gui/viewer/items/Items.py
+++ src/gui/viewer/items/Items.py
@@ -1,8 +1,10 @@
 from enum import Enum
 from typing import Type
 
-from src.gui.viewer.items import Points, Edges, Axes
+from src.gui.viewer.items.Axes import Axes
+from src.gui.viewer.items.Edges import Edges
 from src.gui.viewer.items.GraphicsItem import GraphicsItem
+from src.gui.viewer.items.Points import Points
 
 
 class Items(Enum):
