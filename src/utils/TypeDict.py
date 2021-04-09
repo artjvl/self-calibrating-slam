8a79e2b145914ae14cd4db200a47f18c757890e9
Art <artvanliere@gmail.com>
2021-04-09 02:05:58 +0200

    Added type-hinting in TypeDict class

--- src/utils/TypeDict.py
+++ src/utils/TypeDict.py
@@ -1,22 +1,24 @@
 import typing as tp
 
+T = tp.TypeVar('T')
 
-class TypeDict(object):
+
+class TypeDict(tp.Generic[T]):
     def __init__(self):
-        self._dict: tp.Dict[tp.Type[tp.Any], tp.List[tp.Any]] = {}
+        self._dict: tp.Dict[tp.Type[T], tp.List[T]] = {}
 
-    def add(self, element: tp.Any):
+    def add(self, element: T):
         type_ = type(element)
         if type_ not in self._dict:
             self._dict[type_] = []
         self._dict[type_].append(element)
 
-    def get_types(self) -> tp.List[tp.Type[tp.Any]]:
+    def get_types(self) -> tp.List[tp.Type[T]]:
         return list(self._dict.keys())
 
-    def __contains__(self, type_: tp.Type[tp.Any]) -> bool:
+    def __contains__(self, type_: tp.Type[T]) -> bool:
         return type_ in self._dict
 
-    def __getitem__(self, type_: tp.Type[tp.Any]) -> tp.List[tp.Any]:
+    def __getitem__(self, type_: tp.Type[T]) -> tp.List[T]:
         assert type_ in self
         return self._dict[type_]
