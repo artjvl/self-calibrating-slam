249c648a6ceb12f17b6eae56e36bf546d0a915c7
Art <artvanliere@gmail.com>
2021-04-13 19:42:57 +0200

    Modified simulation implementations

--- src/simulation/hardcoded/HardcodedSimulation.py
+++ src/simulation/hardcoded/HardcodedSimulation.py
@@ -7,6 +7,7 @@ from src.framework.graph.Graph import Graph
 from src.framework.graph.types.scslam2d.nodes.information import InformationNodeFull3
 from src.framework.graph.types.scslam2d.nodes.parameter import ParameterNodeSE2
 from src.framework.math.lie.transformation import SE2
+from src.framework.math.matrix.square import Square3
 from src.framework.math.matrix.vector import Vector6
 from src.framework.simulation.BiSimulation2D import BiSimulation2D
 from src.framework.simulation.sensors.SensorSE2 import SensorSE2
@@ -28,19 +29,19 @@ class HardcodedSimulation(BiSimulation2D):
         par.set_as_bias()
         true = SensorSE2()
         true.add_parameter(par)
-        true.add_information(info)
+        true.set_information(Square3([[800, 0, 0], [0, 600, 0], [0, 0, 400]]))
         self.add_sensor('lidar', true, deepcopy(true))
 
         # self.add_odometry(SE2.from_elements(1., 0.2, 0.4), 'lidar')
         # self.add_odometry(SE2.from_elements(0.2, 1., 0.8), 'lidar')
 
         num: int = 5
-        for _ in range(60):
+        for _ in range(8):
             for __ in range(num):
                 angle: float = np.deg2rad(np.random.choice([90, -90])) if __ == num - 1 else 0.
                 motion = SE2.from_translation_angle_elements(1.0, 0, angle)
                 self.add_odometry(motion, 'lidar')
-                self.add_proximity(5, 'lidar', threshold=0.2)
-                self.add_closure(3., 'lidar', threshold=0.1)
+                self.add_proximity(5, 'lidar', threshold=0.4)
+                self.add_closure(3., 'lidar', threshold=0.4)
 
         return self.get_graphs()
--- src/simulation/tutorial_slam2d/TutorialSimulation.py
+++ src/simulation/tutorial_slam2d/TutorialSimulation.py
@@ -6,6 +6,7 @@ import numpy as np
 from src.framework.graph.types.scslam2d.nodes.information import InformationNodeFull3
 from src.framework.graph.types.scslam2d.nodes.parameter import ParameterNodeSE2
 from src.framework.math.lie.transformation import SE2
+from src.framework.math.matrix.square import Square3
 from src.framework.math.matrix.vector import Vector3, Vector6
 from src.framework.simulation.BiSimulation2D import BiSimulation2D
 from src.framework.simulation.ParameterSet import ParameterSet
@@ -50,17 +51,18 @@ class TutorialSimulation(BiSimulation2D):
         par.set_value(SE2.from_translation_angle_elements(0.2, 0.3, 0.02))
         par.set_as_bias()
         true = SensorSE2()
-        true.add_parameter(par)
-        true.add_information(info)
+        true.set_information(
+            Square3([[1/translation_noise_x, 0, 0], [0, 1/translation_noise_y, 0], [0, 0, 1/rotation_noise_deg]])
+        )
+        # true.add_parameter(par)
+        # true.add_info_node(info)
         self.add_sensor('lidar', true, deepcopy(true))
 
-        angle: float = np.deg2rad(0)
+        num: int = 5
         for _ in range(60):
-            for __ in range(int(parameters['step_count'])):
-                motion = SE2.from_elements(step_length, 0, angle)
-                angle = np.deg2rad(0)
+            for __ in range(num):
+                angle: float = np.deg2rad(np.random.choice([90, -90])) if __ == num - 1 else 0.
+                motion = SE2.from_translation_angle_elements(step_length, 0, angle)
                 self.add_odometry(motion, 'lidar')
                 self.add_proximity(proximity_separation, 'lidar', threshold=proximity_probability)
                 self.add_closure(reach, 'lidar', separation=closure_separation, threshold=closure_probability)
-            angle = np.deg2rad(np.random.choice([90, -90]))
-
