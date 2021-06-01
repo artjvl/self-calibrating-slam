f440d6df6105d8a273142a22759a933923f753ed
Art <artvanliere@gmail.com>
2021-06-01 15:59:34 +0200

    Made Terminal white-on-black

--- src/gui/terminal/TerminalText.py
+++ src/gui/terminal/TerminalText.py
@@ -1,13 +1,12 @@
 import sys
 
-from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import QTextEdit
+from PyQt5 import QtGui, QtWidgets
 
 
 class Stream(object):
 
     # constructor
-    def __init__(self, edit: QTextEdit):
+    def __init__(self, edit: QtWidgets.QTextEdit):
         self.edit = edit
         self.out = sys.__stdout__
 
@@ -20,13 +19,18 @@ class Stream(object):
         pass
 
 
-class TerminalText(QTextEdit):
+class TerminalText(QtWidgets.QTextEdit):
 
     # constructor
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.setFont(QFont('Courier New', 8))
+        self.setFont(QtGui.QFont('Courier New', 8))
         self.setReadOnly(True)
+        p = self.palette()
+        p.setColor(QtGui.QPalette.Base, QtGui.QColor("#2C2C2C"))
+        p.setColor(QtGui.QPalette.Text, QtGui.QColor("#ffffff"))
+        self.setPalette(p)
+
         sys.stdout = Stream(self)
 
     def insertPlainText(self, text: str) -> None:
